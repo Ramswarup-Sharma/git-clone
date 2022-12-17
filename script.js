9f941e76c37af43e3ccb551f12a27fc3de6dab93
Ramswarup sharma <115331810+Ramswarup-Sharma@users.noreply.github.com>
2022-12-17 22:08:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Welcome to JavaScript");
+
+// let ap = document.getElementById('pro');
+// let ap2 = document.getElementById('pro2');
+
+// let earpods = "img/ap1.png";
+// let withCase = "img/ap2b.png";
+
+// ap.addEventListener('click', function () {
+//     document.getElementById('theImage').src = earpods;
+// })
+
+// ap2.addEventListener('click', function () {
+//     document.getElementById('theImage').src = withCase;
+// })
